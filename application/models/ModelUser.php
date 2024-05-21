413ffe19dc9a97b2ef0006156d009a8ddd0a9266
Richarddigaa <richarddigaa@gmail.com>
2024-05-21 15:34:45 +0700

    Menambahkan halaman login dan register

---
+++ application/models/ModelUser.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class ModelUser extends CI_Model
+{
+
+    public function cekData($where = null)
+    {
+        return $this->db->get_where('user', $where);
+    }
+
+    public function simpanData($data = null)
+    {
+        $this->db->insert('user', $data);
+    }
+
+    public function simpanBerdonasi($data = null)
+    {
+        $this->db->insert('user_berdonasi', $data);
+    }
+}
